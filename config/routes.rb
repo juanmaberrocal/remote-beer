6f6d0466696b9bb5b4cad82ee6d0972523853d38
Juanma Berrocal <juanmaberrocal37@gmail.com>
2017-04-24 06:43:33 -0700

    fixed router to work with angular router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   root 'home#index'
   resources :beers
   resources :cities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # catch angular js routing
+  get '*path', to: 'home#index'
 end
